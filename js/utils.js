0b10f0f61bfbdaefbe1f527cbdb67b989fd3604f
PeganovOleg <59733896+PeganovOleg@users.noreply.github.com>
2020-02-17 10:54:41 +0300

    Add files via upload

---
+++ js/utils.js
@@ -0,0 +1,12 @@
+function getTimestamp() {
+  let d = new Date();
+  return d.getTime();
+ 
+}
+
+function randomDivId() {
+  let d = Math.floor(Math.random() * 6) + 1;
+  let n = Math.floor(Math.random() * 6) + 1;
+  return `#slot-${d}${n}`;
+   console.log(`#slot-${d}${n}`);
+}
